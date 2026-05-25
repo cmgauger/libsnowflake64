46a37f8a6335758e0274738524d48a280245ab18
Christian Gauger-Cosgrove <captainkirk359@gmail.com>
2026-05-25 09:08:17 -0400

    Renamed from 'snowflake64' to 'libsnowflake64' as this is a library.

--- build_vstudio.bat
+++ build_vstudio.bat
@@ -1,5 +1,5 @@
 @echo off
-MSBuild /nologo snowflake64.sln /Target:Build /Property:Configuration=Debug /Property:Platform=Win32
-MSBuild /nologo snowflake64.sln /Target:Build /Property:Configuration=Debug /Property:Platform=x64
-MSBuild /nologo snowflake64.sln /Target:Build /Property:Configuration=Release /Property:Platform=Win32
-MSBuild /nologo snowflake64.sln /Target:Build /Property:Configuration=Release /Property:Platform=x64
+MSBuild /nologo libsnowflake64.sln /Target:Build /Property:Configuration=Debug /Property:Platform=Win32
+MSBuild /nologo libsnowflake64.sln /Target:Build /Property:Configuration=Debug /Property:Platform=x64
+MSBuild /nologo libsnowflake64.sln /Target:Build /Property:Configuration=Release /Property:Platform=Win32
+MSBuild /nologo libsnowflake64.sln /Target:Build /Property:Configuration=Release /Property:Platform=x64
